3391c9ccae58a087d235d332ca7ad70cebc1f489
joaoroberto39 <62101070+joaoroberto39@users.noreply.github.com>
2020-10-20 09:49:21 -0300

    Add files via upload

---
+++ AMAAN.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <HEADER>
+      <H2>SOUL COOL BABY SCANDALOUS</H2>
+    </HEADER>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
